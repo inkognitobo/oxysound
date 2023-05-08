7fc5848d367c14fcb14ae040f529ce2639cbb9c3
inkognitobo <tobias.huerten@googlemail.com>
2023-05-08 11:33:57 +0200

    feat: add data structures for videos and playlists and allow saving via serialization

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ mod args;
 mod playlist;
 
 use crate::args::Args;
-use crate::playlist::compose_playlist_url;
+use crate::playlist::{Playlist, Video};
 use clap::Parser;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let arguments = Args::parse();
 
-    println!("{:?}", compose_playlist_url(arguments.ids));
+    let videos: Vec<Video> = arguments
+        .ids
+        .iter()
+        .map(|id| Video::new(id.to_string()))
+        .collect();
+
+    let playlist = Playlist::new("test", videos);
+
+    println!("{:?}", playlist::compose_playlist_url(playlist.videos()));
+
+    playlist::write_to_file(&playlist, "./")?;
+    Ok(())
 }
--- src/playlist.rs
+++ src/playlist.rs
@@ -1,24 +1,89 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+/// Data structure for a playlist
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Playlist {
+    name: String,
+    videos: Vec<Video>,
+}
+
+impl Playlist {
+    /// Constructor
+    pub fn new(name: &str, videos: Vec<Video>) -> Self {
+        let name = name.to_string();
+
+        Playlist { name, videos }
+    }
+
+    pub fn videos(&self) -> &Vec<Video> {
+        &self.videos
+    }
+}
+
+/// Data structure for video meta data
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Video {
+    id: String,
+}
+
+impl Video {
+    /// Constructor
+    pub fn new(id: String) -> Self {
+        Video { id }
+    }
+}
+
 /// Compose the playlist url using the base url and a list of video IDs
-/// * ids - contains a list of valid YouTube video identifiers
-pub fn compose_playlist_url(ids: Vec<String>) -> String {
+/// * videos - array of `Video`s that each contain valid YouTube IDs
+pub fn compose_playlist_url(videos: &[Video]) -> String {
     const BASE_URL: &str = "http://www.youtube.com/watch_videos?video_ids=";
+
+    let ids: Vec<String> = videos.iter().map(|video| video.id.to_string()).collect();
 
     let mut url = String::from(BASE_URL);
     url.push_str(&ids.join(","));
     url
 }
 
+/// Serialize a `Playlist` and write content to a .json file using the playlist's name as file name
+/// * playlist - data structure containing playlist metadata
+/// * file_path - path to the save directory
+pub fn write_to_file(playlist: &Playlist, file_path: &str) -> std::io::Result<()> {
+    let mut file_path = file_path.to_string();
+    file_path.push_str(&playlist.name);
+    file_path.push_str(".json");
+
+    let playlist_json: String = serde_json::to_string(&playlist)
+        .expect("Should be able to convert Playlist to json string");
+
+    fs::write(file_path, &playlist_json)?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::playlist::compose_playlist_url;
+    use crate::playlist::{compose_playlist_url, Playlist, Video};
+
+    #[test]
+    fn test_video_object() {
+        Video::new(String::from("test_id"));
+    }
+
+    #[test]
+    fn test_playlist_object() {
+        Playlist::new("Test", vec![Video::new(String::from("test_id"))]);
+    }
 
     #[test]
     fn test_compose_playlist_url() {
         assert_eq!(
-            compose_playlist_url(vec![
-                String::from("test_id1"),
-                String::from("test_id2"),
-                String::from("test_id3")
+            compose_playlist_url(&[
+                Video::new(String::from("test_id1")),
+                Video::new(String::from("test_id2")),
+                Video::new(String::from("test_id3")),
             ]),
             String::from(
                 "http://www.youtube.com/watch_videos?video_ids=test_id1,test_id2,test_id3"
