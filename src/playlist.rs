1a1308aa0a18539a8dc18168bd14a5035bdfe540
inkognitobo <tobias.huerten@googlemail.com>
2023-05-08 10:14:34 +0200

    test: add basic test

--- src/playlist.rs
+++ src/playlist.rs
@@ -7,3 +7,22 @@ pub fn compose_playlist_url(ids: Vec<String>) -> String {
     url.push_str(&ids.join(","));
     url
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::playlist::compose_playlist_url;
+
+    #[test]
+    fn test_compose_playlist_url() {
+        assert_eq!(
+            compose_playlist_url(vec![
+                String::from("test_id1"),
+                String::from("test_id2"),
+                String::from("test_id3")
+            ]),
+            String::from(
+                "http://www.youtube.com/watch_videos?video_ids=test_id1,test_id2,test_id3"
+            )
+        );
+    }
+}
