e23524599c6950ca3db30b42d3bcc378f17f3681
inkognitobo <tobias.huerten@googlemail.com>
2023-05-28 12:25:45 +0200

    test: add tests

--- src/youtube_api.rs
+++ src/youtube_api.rs
@@ -59,3 +59,85 @@ pub async fn make_video_request(video_ids: &[String]) -> Result<Response> {
         .await?;
     Ok(response)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn rick_astley_response() -> Response {
+        Response {
+            kind: "youtube#videoListResponse".into(),
+            items: vec![ResponseItem {
+                kind: "youtube#video".into(),
+                id: "dQw4w9WgXcQ".into(),
+                snippet: ResponseSnippet {
+                    published_at: Some("2009-10-25T06:57:33Z".into()),
+                    channel_id: Some("UCuAXFkgsw1L7xaCfnd5JJOw".into()),
+                    title: Some(
+                        "Rick Astley - Never Gonna Give You Up (Official Music Video)".into(),
+                    ),
+                    description: Some(
+                        "The official video for “Never Gonna Give You Up” by Rick Astley".into(),
+                    ),
+                    channel_title: Some("Rick Astley".into()),
+                    tags: Some(vec!["".into()]),
+                    category_id: Some("10".into()),
+                },
+            }],
+        }
+    }
+
+    #[test]
+    #[ignore = "test requires API key"]
+    fn test_create_video_request() {
+        let request = create_videos_request(&["dQw4w9WgXcQ".into()])
+            .expect("Expect config to load successfully for test");
+        assert!(request.contains("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics"));
+        assert!(request.contains("&id=dQw4w9WgXcQ"));
+
+        let request = create_videos_request(&["dQw4w9WgXcQ".into(), "y6120QOlsfU".into()])
+            .expect("Expect config to load successfully for test");
+        assert!(request.contains("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics"));
+        assert!(request.contains("&id=dQw4w9WgXcQ,y6120QOlsfU"));
+    }
+
+    #[tokio::test]
+    #[ignore = "test requires API key"]
+    async fn test_make_video_request() {
+        let expected_response = rick_astley_response();
+        let expected_first_item = expected_response
+            .items
+            .first()
+            .expect("Has exactly one item");
+
+        let response = make_video_request(&["dQw4w9WgXcQ".into()])
+            .await
+            .expect("Expect request to succeed for testing purposes");
+        let first_item = response
+            .items
+            .first()
+            .expect("Should have first item if request succeeded");
+
+        assert_eq!(response.kind, expected_response.kind);
+        assert!(!response.items.is_empty());
+        assert_eq!(first_item.id, expected_first_item.id);
+        assert_eq!(first_item.kind, expected_first_item.kind);
+        assert_eq!(
+            first_item.snippet.published_at,
+            expected_first_item.snippet.published_at
+        );
+        assert_eq!(
+            first_item.snippet.channel_id,
+            expected_first_item.snippet.channel_id
+        );
+        assert_eq!(first_item.snippet.title, expected_first_item.snippet.title);
+        assert_eq!(
+            first_item.snippet.channel_title,
+            expected_first_item.snippet.channel_title
+        );
+        assert_eq!(
+            first_item.snippet.category_id,
+            expected_first_item.snippet.category_id
+        );
+    }
+}
